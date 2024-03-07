64c589f0ff60deee29590f8f39c3a0bfd6084805
Falsedeer <93675399+Falsedeer@users.noreply.github.com>
2024-03-07 14:10:25 +0800

    Update Library Files

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Py-KeyLogger',
+    version='0.1.0-alpha',
+    author="Aurvandill Lab",
+    author_email='aurvandillresearch@gmail.com',
+    description='A multithreaded keylogger template based on low-level keyboard hooks for python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Aurvandill-Lab/Python-KeyLogger-Lib',
+    packages=find_packages(exclude=('tests*', 'testing*')),
+    install_requires=[
+        # List your project's dependencies here.
+        "ctypes",
+    ],
+    classifiers=[
+        'Development Status :: 0.1.0 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
